8c82f1a4be91c7d8de8114baebd17912d876d613
William Bryce Angell <bryce.angell@resi.io>
2022-06-23 21:49:30 -0500

    add loading and error to useFetch

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -1,5 +1,5 @@
 import "./TripList.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 type Trip = { id: string; title: string; price: string; loc: string };
@@ -7,28 +7,38 @@ type Trip = { id: string; title: string; price: string; loc: string };
 interface Props {}
 
 const TripList: React.FC<Props> = ({}) => {
-  const tripsUrl = "http://localhost:3001/trips";
+  const tripsUrl = "http://localhost:3001/trips/invalid";
   const [url, setUrl] = useState(tripsUrl);
-  const { data: trips }: { data: Array<Trip> } = useFetch(url);
+  const { data, isPending, error } = useFetch(url);
+
+  useEffect(() => {
+    if (error instanceof Error) console.warn(error);
+  }, [error]);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
-      <ul>
-        {trips &&
-          trips.map((trip) => (
-            <li key={trip.id}>
-              <h3>{trip.title}</h3>
-              <p>{trip.price}</p>
-            </li>
-          ))}
-      </ul>
-      <div className="filters">
-        <button onClick={() => setUrl(`${tripsUrl}?loc=europe`)}>
-          European Trips
-        </button>
-        <button onClick={() => setUrl(tripsUrl)}>All Trips</button>
-      </div>
+      {isPending && <div>Loading trips...</div>}
+      {!isPending && error && <div>Failed to load trips.</div>}
+      {!isPending && (
+        <ul>
+          {data &&
+            data.map((trip: Trip) => (
+              <li key={trip.id}>
+                <h3>{trip.title}</h3>
+                <p>{trip.price}</p>
+              </li>
+            ))}
+        </ul>
+      )}
+      {!isPending && !error && (
+        <div className="filters">
+          <button onClick={() => setUrl(`${tripsUrl}?loc=europe`)}>
+            European Trips
+          </button>
+          <button onClick={() => setUrl(tripsUrl)}>All Trips</button>
+        </div>
+      )}
     </div>
   );
 };
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 
 const useFetch = (url: string) => {
   const [data, setData] = useState<any>(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
   useEffect(() => {
     (async () => {
-      const response = await fetch(url);
-      const json = await response.json();
-      setData(json);
+      setIsPending(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(response.statusText);
+        const json = await response.json();
+        setData(json);
+        setError(null);
+      } catch (e) {
+        setError(e as Error);
+      } finally {
+        setIsPending(false);
+      }
     })();
   }, [url]);
-  return { data };
+
+  return { data, isPending, error };
 };
 
 export default useFetch;
